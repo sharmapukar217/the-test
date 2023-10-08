0f45e0ae70a1c7f55c381c6ad978e5aa76440f5a
It's me <sharmapukar217@gmail.com>
2023-10-08 22:57:25 +0545

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+async function main() {
+   const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', // Path to your Microsoft Edge executable
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://web.whatsapp.com');
+
+  await page.waitForTimeout(10000);
+  await page.screenshot({ path: 'screenshot.png' });
+
+
+  await browser.close();
+}
+
+
+main().catch(console.error);
