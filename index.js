a63619fb532fc37525da14e014c5b43e393f7068
It's me <sharmapukar217@gmail.com>
2023-10-09 01:38:04 +0545

    done

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const puppeteer = require('puppeteer');
 
 async function main() {
    const browser = await puppeteer.launch({
-    headless: true,
+    headless: "new",
     executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', // Path to your Microsoft Edge executable
   });
 
   const page = await browser.newPage();
-  await page.goto('https://instagram.com');
+  await page.goto('https://web.whatsapp.com');
 
   await page.waitForTimeout(10000);
   await page.screenshot({ path: 'screenshot.png' });
